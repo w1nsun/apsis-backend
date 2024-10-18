c90dcc55540c9c61ce3f21d042105de22da172fb
w1nsun <ilya.nevmyvaka@gmail.com>
2024-10-18 11:53:34 +0200

    change port

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document, { useGlobalPrefix: true });
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 80);
 }
 bootstrap();
